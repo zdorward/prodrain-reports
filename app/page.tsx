cf5e29b287b65e170bd78d18ade5e135244b49eb
Zack Dorward <zdorward1@gmail.com>
2025-11-26 21:46:28 -0700

    small tweak

--- app/page.tsx
+++ app/page.tsx
@@ -178,7 +178,7 @@ export default function HomePage() {
             type="submit"
             className="cursor-pointer inline-flex items-center justify-center rounded-md bg-slate-800 px-4 py-2 text-sm font-medium text-white hover:bg-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2"
           >
-            View Draft
+            Generate Report
           </button>
         </form>
       </div>
